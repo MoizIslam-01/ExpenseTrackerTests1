b761dad5fb2486e4026a4204c6547eff43a984df
Moiz Islam <smonarch421@gmail.com>
2025-12-19 19:47:21 +0500

    Update test URL for unknown route redirection

--- tests/test_page_not_found.py
+++ tests/test_page_not_found.py
@@ -3,7 +3,7 @@ from test_setup import get_driver
 def test_unknown_route_redirects_to_login():
     driver = get_driver()
     try:
-        driver.get("http://13.126.82.141:3000/unknown-route")
+        driver.get("http://65.1.107.51:3000/unknown-route")
         # check if login page elements exist
         assert "Login" in driver.page_source or driver.find_element("id", "login-btn")
     finally:
